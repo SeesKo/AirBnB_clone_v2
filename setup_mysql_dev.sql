8c1ba97fb8669f5d88349eeb63aae4ccc2b2f17b
Siskosee <seeskosee@seeskoseesko.com>
2024-03-16 15:07:10 -0700

    prepares mysql server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Creates or uses the database hbnb_dev_db
+-- User should have all privileges on the database
+-- Grants SELECT privilege on database performance_schema
+-- Should not fail if database or user already exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
