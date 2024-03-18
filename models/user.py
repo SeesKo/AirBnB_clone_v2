19d2260dbc11aaa3ff8b6dae032319e5b53247b6
Siskosee <seeskosee@seeskoseesko.com>
2024-03-18 06:28:43 -0700

    user update

--- models/user.py
+++ models/user.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -14,6 +15,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade="all, delete", backref="user")
     else:
         email = ''
         password = ''
