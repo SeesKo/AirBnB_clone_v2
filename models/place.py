8fa795c23b5424a2a33738d46f0724f564c4b369
Siskosee <seeskosee@seeskoseesko.com>
2024-03-18 06:44:48 -0700

    place update

--- models/place.py
+++ models/place.py
@@ -1,28 +1,67 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
+from models.amenity import Amenity
+from models.review import Review
 from sqlalchemy.orm import relationship
+from os import getenv
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+    place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column(
+            'place_id',
+            String(60),
+            ForeignKey('places.id'),
+            primary_key=True, nullable=False),
+        Column(
+            'amenity_id',
+            String(60),
+            ForeignKey('amenities.id'),
+            primary_key=True,
+            nullable=False)
+        )
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-
-    # Defining relationship between Place and Review
-    reviews = relationship("Review", backref="place",
-                           cascade="all, delete-orphan")
-    # Defining relationship between Place and Amenity
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenities = relationship(
+            "Amenity",
+            secondary='place_amenity',
+            back_populates='place_amenities',
+            viewonly=False
+            )
+        reviews = relationship(
+            'Review',
+            backref='place',
+            cascade='all, delete-orphan'
+        )
+    else:
+        """ A place to stay """
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
