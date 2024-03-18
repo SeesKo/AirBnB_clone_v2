b1905f13aab60387c82f51003d61b8da322f4821
Siskosee <seeskosee@seeskoseesko.com>
2024-03-18 06:57:17 -0700

    place update

--- models/place.py
+++ models/place.py
@@ -28,9 +28,9 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    __tablename__ = 'places'
 
-        __tablename__ = 'places'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
