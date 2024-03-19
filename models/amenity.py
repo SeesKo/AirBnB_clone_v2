b81f5440ac8e312aa148809ab03a9838fceace81
Siskosee <seeskosee@seeskoseesko.com>
2024-03-19 04:55:40 -0700

    amenities update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Amenity(BaseModel):
-    name = ""
+    """Amenity class for representing amenities """
+    __tablename__ = 'amenities'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity")
+    else:
+        name = ""
