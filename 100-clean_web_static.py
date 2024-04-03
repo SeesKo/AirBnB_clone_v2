8768b02f9508f02c7ae4ec6538a772f1e94a884a
Siskosee <seeskosee@seeskoseesko.com>
2024-04-03 14:10:35 -0700

    deleting outdated archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -14,12 +14,49 @@ def do_clean(number=0):
     """
     Deletes out-of-date archives
     """
+    try:
+        number = int(number)
+        if number < 0:
+            number = 0
 
-    if number == 0:
-        number = 2
-    else:
-        number += 1
+        versions_path = "/data/web_static/releases/"
+        archives_path = "versions/"
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+        with lcd(archives_path):
+            local_archives = [f for f in listdir(".") if isfile(join(".", f))]
+
+        local_archives.sort(reverse=True)
+
+        if number == 0:
+            number = 1
+
+        archives_to_keep = local_archives[:number]
+
+        with cd(versions_path):
+            remote_archives = run("ls -1t | grep -E '^web_static_.*.tgz$'").split('\n')
+
+        remote_archives.sort(reverse=True)
+
+        if number == 0:
+            number = 1
+
+        remote_archives_to_keep = remote_archives[:number]
+
+        for archive in local_archives:
+            if archive not in archives_to_keep:
+                local("rm -f {}".format(archive))
+
+        for archive in remote_archives:
+            if archive not in remote_archives_to_keep:
+                run("rm -f {}".format(archive))
+
+    except Exception as e:
+        print("Error: {}".format(e))
+        return False
+
+    return True
+
+
+if __name__ == "__main__":
+    import sys
+    do_clean(*sys.argv[1:])
