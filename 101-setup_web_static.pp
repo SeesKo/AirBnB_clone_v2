f3d13f4f39120e69a0b96d8c32a32f7569e12697
Siskosee <seeskosee@seeskoseesko.com>
2024-04-03 15:10:42 -0700

    nginx serving static files

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,62 +1,73 @@
 # Puppet manifest to configure Nginx to serve static files
 
-exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['start Nginx'],
+# Define directories
+file { '/data':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-exec {'start Nginx':
-  provider => shell,
-  command  => 'sudo service nginx start',
-  before   => Exec['create first directory'],
+file { '/data/web_static':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-exec {'create first directory':
-  provider => shell,
-  command  => 'sudo mkdir -p /data/web_static/releases/test/',
-  before   => Exec['create second directory'],
+file { '/data/web_static/releases':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-exec {'create second directory':
-  provider => shell,
-  command  => 'sudo mkdir -p /data/web_static/shared/',
-  before   => Exec['content into html'],
+file { '/data/web_static/shared':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-exec {'content into html':
-  provider => shell,
-  command  => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
-  before   => Exec['symbolic link'],
+file { '/data/web_static/releases/test':
+  ensure => directory,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-exec {'symbolic link':
-  provider => shell,
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  before   => Exec['put location'],
-}
-
-exec {'put location':
-  provider => shell,
-  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
-  before   => Exec['restart Nginx'],
-}
-
-exec {'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-  before   => File['/data/']
-}
-
-file {'/data/':
-  ensure  => directory,
+# Create index.html file
+file { '/data/web_static/releases/test/index.html':
+  ensure  => file,
+  content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>',
   owner   => 'ubuntu',
   group   => 'ubuntu',
-  recurse => true,
+}
+
+# Create symbolic link
+file { '/data/web_static/current':
+  ensure  => link,
+  target  => '/data/web_static/releases/test',
+  owner   => 'root',
+  group   => 'root',
+}
+
+# Configure Nginx to serve static files
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => template('path/to/nginx_config_template.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
 }
