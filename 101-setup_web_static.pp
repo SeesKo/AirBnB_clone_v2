bef9be5c8458c487182e05900504fd65060f97f1
Siskosee <seeskosee@seeskoseesko.com>
2024-04-03 20:39:49 -0700

    nginx serving static files

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,65 +1,78 @@
 # Puppet manifest to configure Nginx to serve static files
 
 # Nginx configuration
-$nginx_conf = "server {
+$nginx_configuration = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
-    location /hbnb_static {
+
+    # Redirect block
+    location /redirect_here {
+        return 301 https://example.com;
+    }
+
+    # Static content block
+    location /static_content {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-    location /redirect_me {
-        return 301 https://th3-gr00t.tk;
-    }
-    error_page 404 /404.html;
+
+    # Error handling
+    error_page 404 /404_page.html;
     location /404 {
       root /var/www/html;
       internal;
     }
 }"
 
+# Install Nginx package
 package { 'nginx':
-  ensure   => 'present',
+  ensure   => 'installed',
   provider => 'apt'
 } ->
 
-file { '/data':
+# Create directories
+file { '/data_directory':
   ensure  => 'directory'
 } ->
 
-file { '/data/web_static':
+file { '/data_directory/web_static_directory':
   ensure => 'directory'
 } ->
 
-file { '/data/web_static/releases':
+file { '/data_directory/web_static_directory/releases':
   ensure => 'directory'
 } ->
 
-file { '/data/web_static/releases/test':
+file { '/data_directory/web_static_directory/releases/test':
   ensure => 'directory'
 } ->
 
-file { '/data/web_static/shared':
+file { '/data_directory/web_static_directory/shared':
   ensure => 'directory'
 } ->
 
-file { '/data/web_static/releases/test/index.html':
+# Create a fake HTML file
+file { '/data_directory/web_static_directory/releases/test/fake_index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n"
+  content => "Holberton School\n"
 } ->
 
-file { '/data/web_static/current':
+# Create symbolic link
+file { '/data_directory/web_static_directory/symlink_to_current':
   ensure => 'link',
-  target => '/data/web_static/releases/test'
+  target => '/data_directory/web_static_directory/releases/test'
 } ->
 
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+# Change ownership
+exec { 'change_ownership':
+  command => 'chown -R ubuntu:ubuntu /data_directory/',
+  path    => ['/usr/bin', '/bin'],
 }
 
+# Create directories for web server
 file { '/var/www':
   ensure => 'directory'
 } ->
@@ -68,21 +81,26 @@ file { '/var/www/html':
   ensure => 'directory'
 } ->
 
+# Create index.html file
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n"
+  content => "Holberton School\n"
 } ->
 
-file { '/var/www/html/404.html':
+# Create custom 404 page
+file { '/var/www/html/404_page.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
+  content => "This is a custom 404 page\n"
 } ->
 
+# Configure Nginx
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $nginx_conf
+  content => $nginx_configuration
 } ->
 
-exec { 'nginx restart':
-  path => '/etc/init.d/'
+# Restart Nginx
+exec { 'restart_nginx':
+  command => '/etc/init.d/nginx restart',
+  path    => ['/usr/sbin', '/usr/bin', '/sbin', '/bin'],
 }
