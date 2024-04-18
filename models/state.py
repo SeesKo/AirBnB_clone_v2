30cad559948ce3fb1e641a08fa25d2c8a2e8997d
SeesKo <seeskosee@seeskome.com>
2024-04-18 16:25:31 +0000

    states update

--- models/state.py
+++ models/state.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if models.storage_t == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ Initialization of State class """
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            """Getter attribute that returns the list of City instances"""
-            from models import storage
+            """ Getter attribute that returns the list of City instances """
             city_list = []
-            for city in storage.all("City").values():
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
